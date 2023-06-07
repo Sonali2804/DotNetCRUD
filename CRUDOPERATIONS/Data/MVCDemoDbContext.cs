c2ed344810921cb91c127d71b715e5d54315dc14
Sonali2804 <kumarisonali219@gmail.com>
2023-06-07 13:13:38 +0530

    First Commit

---
+++ CRUDOPERATIONS/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDOPERATIONS.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDOPERATIONS.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+       //  This will be the same name as table
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
